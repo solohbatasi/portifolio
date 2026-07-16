c8f521068bc79f0d63e470ade87ba4cdf86885ea
solohbatasi <solomonbatasi@gmail.com>
2026-07-16 15:33:24 +0300

    sessions table

--- backend/database/migrations/0001_01_01_000001_create_cache_table.php
+++ backend/database/migrations/0001_01_01_000001_create_cache_table.php
@@ -22,6 +22,14 @@ return new class extends Migration
             $table->string('owner');
             $table->bigInteger('expiration')->index();
         });
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     /**
